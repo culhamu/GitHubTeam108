091ee52fc9ee020e0f8fe88795dad7659028b53f
Mustafa Çulha <your_merguven75@mail.com>
2022-12-21 20:25:58 +0300

    v3

--- src/C01.java
+++ src/C01.java
@@ -2,5 +2,6 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("hello gitHub");
         System.out.println("CMD çok kullanışlı ....");
+        System.out.println("Güzel günler gelecek inşallah");
     }
 }
